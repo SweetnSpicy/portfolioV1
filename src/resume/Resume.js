764d5119b110bc923d0b99ddeb9354e3cd755fc7
Cauldierre McKay <cxm1544@rit.edu>
2026-01-07 22:06:32 -0500

    Update Resume.js

--- src/resume/Resume.js
+++ src/resume/Resume.js
@@ -7,6 +7,10 @@ import './Resume.scss';
  * If no file uploaded, ask to import a file
  * if file not .pdf suffix, error and show no file screen again
  */
+
+/**
+ * fred's playground
+ */
 class Resume extends React.Component{
     
 }

c1b8cf47d51e19695aeb7488824c9c2dd13bfa2c
Frederic-Yao <fredericyao@outlook.com>
2026-01-07 22:08:32 -0500

    added comment

--- src/resume/Resume.js
+++ src/resume/Resume.js
@@ -7,6 +7,12 @@ import './Resume.scss';
  * If no file uploaded, ask to import a file
  * if file not .pdf suffix, error and show no file screen again
  */
+/**
+ * Class created to display the file that is uploaded
+ * Specifically a pdf file
+ * If no file uploaded, ask to import a file
+ * if file not .pdf suffix, error and show no file screen again
+ */
 class Resume extends React.Component{
     
 }
